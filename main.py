b30646773e33ea3458f19043354cfe802c34170e
They call me many names <88290402+quixt@users.noreply.github.com>
2022-12-13 12:56:01 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
+import ffmpeg
 import pyaudio;
 import sys;
 import wave
 import threading
 import time
 import keyboard
+import shutil
+import os
+import pickle
+
+
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QMainWindow
+import PyQt6.QtWidgets as QtWidgets
+from PyQt6.QtCore import Qt
+from PyQt6.QtCore import pyqtSlot
+from PyQt6 import QtGui
+soundsChoiceFile = None
+try:
+    soundsChoiceFile = pickle.load(open("sounds.pickle", "rb"))
+except:
+    soundsChoiceFile = [None, None, None, None, None, None, None, None, None, None]
+    pickle.dump(soundsChoiceFile, open("sounds.pickle", "wb"))
+#Initialize the pyaudio object
 p = pyaudio.PyAudio()
 
+#Create the pyqt app (for selecting soundboard)
+app = QApplication([])
+
+
+#Get the device information for selecting the output and input devices
 info = p.get_host_api_info_by_index(0)
 numdevices = info.get('deviceCount')
 
@@ -20,32 +43,41 @@ for i in range(0, numdevices):
 
 
 class Soundboard:
-    def __init__(self, p):
+    def __init__(self, p, audioFiles):
         self.p = p
         self.CHUNK = 1024
         self.FORMAT = pyaudio.paInt16
         self.soundThreads = []
         self.micThreads = []
-        keyboard.add_hotkey('ctrl+alt+1', self.createStream1)
+        self.audioFiles = audioFiles
+        self.startHookHotkeys()
         keyboard.add_hotkey('ctrl+alt+p', self.pauseAll)
         keyboard.add_hotkey('ctrl+alt+r', self.resumeAll)
         keyboard.add_hotkey('ctrl+alt+s', self.cancelAll)
-
+        # self.audioFiles = [None, None, None, None, None, None, None, None, None, None]
+        
         for i in range(0,19):
             self.soundThreads.append(None)
             self.micThreads.append(None)
         self.micThread = threading.Thread(target=self.micInput).start()
+    def startHookHotkeys(self):
+        for i in range(0,9):
+            self.hookHotkey(i)
+    def unhookHotkey(self, i):
+        keyboard.remove_hotkey('ctrl+alt+'+str(i+1))
+    def hookHotkey(self,i):
+        keyboard.add_hotkey('ctrl+alt+'+str(i+1), self.createStream,args=(self.audioFiles[i],))
     def micInput(self):
         micStream = p.open(format=pyaudio.paInt16,rate=44100,channels=2,input=True,frames_per_buffer=1024)
         outputStream = p.open(format=pyaudio.paInt16,rate=44100,channels=2,output=True,output_device_index=5,frames_per_buffer=1024)
         while True:
             data = micStream.read(self.CHUNK)
             outputStream.write(data)
-    def createStream1(self):
-        if(self.getRecentStream() != None):
+    def createStream(self, filename:str):
+        if(self.getRecentStream() != None and filename != None):
             print("Starting...")
-            threading.Thread(target=self.playSoundOverSpeakers,args=("bettercallsaul.wav",self.getRecentStream(),)).start()
-            threading.Thread(target=self.playSoundOverMic,args=("bettercallsaul.wav",self.getRecentStream(),)).start()
+            threading.Thread(target=self.playSoundOverSpeakers,args=(filename,self.getRecentStream(),)).start()
+            threading.Thread(target=self.playSoundOverMic,args=(filename,self.getRecentStream(),)).start()
             #threading.Thread(target=self.playSoundOverMic,args=("freebirdsolo.wav",)).start()
     def resumeAll(self):
         for i in range(0,19):
@@ -112,23 +144,89 @@ class Soundboard:
         self.micThreads[recentStream] = None
         exit()
 
+
+    
+class Application(QWidget):
+    def __init__(self):
+        super().__init__()
+        global soundsChoiceFile
+        self.setWindowTitle("Audi-Joe Board")
+        self.setGeometry(100, 100, 1000, 500)
+        titleMessage = QLabel("<h1>AUDI-JOE BOARD</h1>", parent=self)
+        titleMessage.move(10, 10)
+        uploadButton = QtWidgets.QPushButton("UPLOAD", parent=self)
+        uploadButton.clicked.connect(self.fileUpload)
+        uploadButton.resize(100, 50)
+        uploadButton.move(10, 100)
+        self.fileComboBox = QtWidgets.QComboBox(self)
+        self.fileComboBox.move(10, 70)
+        self.setComboItems()
+        self.textLabels = []
+        self.setSoundButtons = []
+        for i in range(1,10):
+            self.setSoundButtons.append(QtWidgets.QPushButton("SET SOUND " + str(i), parent=self))
+            self.textLabels.append(QtWidgets.QLabel(str(soundsChoiceFile[i-1]), parent=self))
+            self.textLabels[i-1].resize(95, 20)
+            # if(i < 5):
+            #     self.layout1.addWidget(self.setSoundButtons[i-1])
+            # else:
+            #     self.layout2.addWidget(self.setSoundButtons[i-1])
+            self.setSoundButtons[i-1].resize(100, 50)
+            if i > 5:
+                self.textLabels[i-1].move(200 + ((i-1)%5 * 100), 330)
+                self.setSoundButtons[i-1].move(200 + ((i-1)%5 * 100), 280)
+            else:
+                self.textLabels[i-1].move(200 + ((i-1) * 100), 250)
+                self.setSoundButtons[i-1].move(200 + ((i-1) * 100), 200)
+            
+        self.setSoundButtons[0].clicked.connect(lambda: self.setSound(0))
+        self.setSoundButtons[1].clicked.connect(lambda: self.setSound(1))
+        self.setSoundButtons[2].clicked.connect(lambda: self.setSound(2))
+        self.setSoundButtons[3].clicked.connect(lambda: self.setSound(3))
+        self.setSoundButtons[4].clicked.connect(lambda: self.setSound(4))
+        self.setSoundButtons[5].clicked.connect(lambda: self.setSound(5))
+        self.setSoundButtons[6].clicked.connect(lambda: self.setSound(6))
+        self.setSoundButtons[7].clicked.connect(lambda: self.setSound(7))
+        self.setSoundButtons[8].clicked.connect(lambda: self.setSound(8))
+
+    
+    @pyqtSlot()
+    def setSound(self,l):
+        print(l)
+        soundboard.audioFiles[l] = self.fileComboBox.currentText()
+        self.textLabels[l].setText(self.fileComboBox.currentText())
+        soundboard.unhookHotkey(l)
+        soundboard.hookHotkey(l)
+        pickle.dump(soundboard.audioFiles, open("sounds.pickle", "wb"))
+        print(soundboard.audioFiles)
+    def setComboItems(self):
+        self.fileComboBox.addItems(self.getFiles())
+    def getFiles(self):
+        files = []
+        for x in os.listdir():
+            if x.endswith(".wav"):
+                files.append(x)
+        return files
         
+    @pyqtSlot()
+    def fileUpload(self):
+        filename = QtWidgets.QFileDialog.getOpenFileName(self,'Open File', 'Downloads', "Audio Files (*.wav)")
+        if(filename[0] != ""):
+            
+            file = filename[0].split("/",-1)[-1]
+            if(file.split(".",-1)[-1] == "wav"):
+                print("File: " + file)
+                files = []
+                for x in os.listdir():
+                    if x.endswith(".wav"):
+                        files.append(x)
+                if(filename not in files):
+                    shutil.copy(filename[0], ".")
+        self.setComboItems()
 
-def stayOpen():
-    while True:
-        time.sleep(1)
+soundboard = Soundboard(p, soundsChoiceFile)
+# threading.Thread(target=stayOpen).start()
+window = Application()
+window.show()
 
-
-soundboard = Soundboard(p)
-threading.Thread(target=stayOpen).start()
-# micThread = threading.Thread(target=outputFreeBirdOverMic)
-# micThread.start()
-
-# stream = p.open(format=p.get_format_from_width(wf.getsampwidth()), channels=wf.getnchannels(), rate=wf.getframerate(), output=True, frames_per_buffer=1024, output_device_index=5)
-# data = wf.readframes(CHUNK)
-# while len(data):
-#     stream.write(data)
-#     data = wf.readframes(CHUNK)
-
-# stream.stop_stream()
-# stream.close()
+sys.exit(app.exec())
